ad58980d4149e0712736ac62afa85c25ab7ee9b9
Xiaomai <xsbugh@gmail.com>
2022-07-23 14:57:39 +0800

    创建项目，建模面包底座、肉、起司、黄瓜

---
+++ Assets/Scripts/MeatTexture.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatTexture : MonoBehaviour
+{
+    public List<Texture2D> MeatLevelTexture;
+    private int currentLevel = 0;
+
+    private Material meatMaterial;
+
+    private void Awake() {
+        meatMaterial = GetComponent<Renderer>().material;
+        meatMaterial.mainTexture = MeatLevelTexture[currentLevel];
+    }
+
+    private void Update() {
+        // DEBUG: On mouse click, change MeatLevelTexture
+        if (Input.GetMouseButtonDown(0)) {
+            currentLevel++;
+            if (currentLevel >= MeatLevelTexture.Count) {
+                currentLevel = 0;
+            }
+            meatMaterial.mainTexture = MeatLevelTexture[currentLevel];
+        }
+    }
+}
